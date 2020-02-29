f6141a56fb7b0c634e8344f83a0725991290c5c7
Zhen Zhang <zhangzhen.dev@gmail.com>
2020-02-29 00:00:47 -0500

    added get_info

--- include/unit_common.h
+++ include/unit_common.h
@@ -51,7 +51,60 @@ struct test_entry {
 	char *desc;
 };
 
-void ft_unit_usage(char *name, char *desc);
-int run_tests(struct test_entry *test_array, char *err_buf);
+void ft_unit_usage(char *name, char *desc)
+{
+    fprintf(stderr, "Usage:\n");
+    fprintf(stderr, "  %s [OPTIONS]\n", name);
+
+    if (desc)
+        fprintf(stderr, "\n%s\n", desc);
+
+    fprintf(stderr, "\nOptions:\n");
+    FT_PRINT_OPTS_USAGE("-f <fabric_name>", "specific fabric to use");
+    FT_PRINT_OPTS_USAGE("-d <domain>", "domain name");
+    FT_PRINT_OPTS_USAGE("-p <provider_name>", "specific provider name eg sockets, verbs");
+    FT_PRINT_OPTS_USAGE("-h", "display this help output");
+}
+
+int
+run_tests(struct test_entry *test_array, char *err_buf)
+{
+    int ret;
+    struct test_entry *tep;
+    int failed;
+
+    failed = 0;
+
+    tep = test_array;
+    while (tep->test != NULL) {
+        printf("Running %s [%s]...", tep->name, tep->desc);
+        fflush(stdout);
+        ret = tep->test();
+        switch (ret) {
+            case PASS:
+                printf("PASS!\n");
+                break;
+            case FAIL:
+                printf("FAIL: %s\n", err_buf);
+                failed++;
+                break;
+            case SKIPPED:
+                printf("skipped because: %s\n", err_buf);
+                break;
+            case NOTSUPP:
+                printf("requires unsupported feature: %s\n", err_buf);
+                break;
+            default:
+                printf("FATAL: unexpected code: %d\n", ret);
+                return failed + 1;
+                break;
+        }
+
+        ++tep;
+    }
+
+    return failed;
+}
+
 
 #endif /* _UNIT_COMMON_H_ */
